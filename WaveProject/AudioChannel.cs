33d0885f1d4b465d6a7cfa95a848a2a00b069dba
Joshua Abe <joshua.t.abe@gmail.com>
2017-09-14 20:30:06 -0700

    Pushing all files

---
+++ WaveProject/AudioChannel.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WaveProject {
+    class AudioChannel {
+
+        public AudioChannel() : this("Audio Channel") {
+        }
+
+        public AudioChannel(String name) : this(name, 0, 0) {
+        }
+
+        public AudioChannel(String name, int xPos, int yPos) : this(name, xPos, yPos, 100, 100) {
+        }
+
+        public AudioChannel(String name, int xPos, int yPos, int width, int height) {
+            this.chart = new Chart();
+            this.chartArea = new ChartArea();
+            this.series = new Series();
+
+            Init(name, xPos, yPos, width, height);
+        }
+
+        private void Init(String name, int xPos, int yPos, int width, int height) {
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            InitChartArea(name);
+            InitSeries();
+            InitChart(name, xPos, yPos, width, height);
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+        }
+
+        private void InitChartArea(String name) {
+            chartArea.AxisX.LabelStyle.ForeColor = System.Drawing.Color.Green;
+            chartArea.AxisX.MajorGrid.LineWidth = 0;
+            chartArea.AxisX.Minimum = 0D;
+            chartArea.AxisY.LabelStyle.ForeColor = Color.Green;
+            chartArea.AxisY.MajorGrid.LineWidth = 0;
+            chartArea.AxisY.Title = name;
+            chartArea.AxisY.TitleForeColor = Color.Red;
+            chartArea.BackColor = Color.Black;
+            chartArea.CursorX.IsUserEnabled = true;
+            chartArea.CursorX.IsUserSelectionEnabled = true;
+            chartArea.Name = "ChartArea";
+
+            chart.ChartAreas.Add(chartArea);
+        }
+
+        private void InitSeries() {
+            series.BorderWidth = 2;
+            series.ChartArea = "ChartArea";
+            series.ChartType = SeriesChartType.Spline;
+            series.Color = Color.Green;
+            series.Name = "Series";
+            series.Points.AddXY(0, 120);
+            series.Points.AddXY(1, 0);
+
+            chart.Series.Add(series);
+        }
+
+        private void InitChart(String name, int xPos, int yPos, int width, int height) {
+            chart.Size = new Size(width, height);
+            chart.TabIndex = 1;
+            chart.Text = name;
+            chart.Location = new Point(xPos, yPos);
+            chart.Name = name;
+            chart.BackColor = Color.Black;
+        }
+
+        private Chart chart;
+        private ChartArea chartArea;
+        private Series series;
+
+        public Chart Chart {
+            get {
+                return chart;
+            }
+            set {
+                chart = value;
+            }
+        }
+
+        public Series Series {
+            get {
+                return series;
+            }
+            set {
+                series = value;
+            }
+        }
+
+        public ChartArea ChartArea {
+            get {
+                return chartArea;
+            }
+            set {
+                chartArea = value;
+            }
+        }
+    }
+}
---
+++ WaveProject/Window.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WaveProject {
+    partial class Window {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            ac = new AudioChannel();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint1 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, -128D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint2 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(1D, 0D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint3 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(2D, 128D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint4 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(3D, 0D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint5 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(4D, -128D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint6 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(5D, 0D);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Black;
+            chartArea1.AxisX.LabelStyle.ForeColor = System.Drawing.Color.Green;
+            chartArea1.AxisX.MajorGrid.LineWidth = 0;
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisY.LabelStyle.ForeColor = System.Drawing.Color.Green;
+            chartArea1.AxisY.MajorGrid.LineWidth = 0;
+            chartArea1.AxisY.Title = "L Channel";
+            chartArea1.AxisY.TitleForeColor = System.Drawing.Color.Red;
+            chartArea1.BackColor = System.Drawing.Color.Black;
+            chartArea1.CursorX.IsUserEnabled = true;
+            chartArea1.CursorX.IsUserSelectionEnabled = true;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.Green;
+            series1.Name = "Series1";
+            series1.Points.Add(dataPoint1);
+            series1.Points.Add(dataPoint2);
+            series1.Points.Add(dataPoint3);
+            series1.Points.Add(dataPoint4);
+            series1.Points.Add(dataPoint5);
+            series1.Points.Add(dataPoint6);
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(970, 300);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 772);
+            this.Controls.Add(ac.Chart);
+            this.Name = "Window";
+            this.Text = "Sound Project";
+            this.Load += new System.EventHandler(this.Window_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private AudioChannel ac;
+    }
+}
+
